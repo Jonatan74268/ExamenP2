c29a486c295524cb6900a8acb33403c1053bb90a
Petersito <pog@udelasalle.edu.mx>
2022-05-12 14:24:16 -0500

    se supone que ya jalo

---
+++ src/service-layer/tasks/DeleteCampeonTask.ts
@@ -0,0 +1,22 @@
+import Campeon from '../../domain-layer/entities/Campeon';
+import DatabaseConnection from '../../persistence-layer/DatabaseConnection';
+import IAsyncTask from './IAsyncTask';
+
+//no salia la eliminacion krnal que onda con eso 
+
+export default class DeleteCampeonTask implements IAsyncTask<void> {
+  private campeonId: number;
+  
+  public constructor(campeonId: number) {
+    this.campeonId = campeonId;
+  }
+
+  public async execute(): Promise<void> {
+    const databaseConnection = await DatabaseConnection.getInstance();
+    const campeonRepository = databaseConnection.getRepository(Campeon);
+
+    await campeonRepository.delete(this.campeonId);
+
+
+  }
+}
