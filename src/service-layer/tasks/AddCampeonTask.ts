c62d93052821d738ad8af65cd3bbadad1a91ce9a
Diego A. Romero Ulloa <diegoru428@gmail.com>
2022-05-12 12:52:15 -0500

    AddTask Adaptado
    Se adapto el archivo AdCampeonTask.ts para funcionalidad con la adaptacion de la base de datos.
    Si tienen chancce chequen que no haya errores por favor

--- src/service-layer/tasks/AddCampeonTask.ts
+++ src/service-layer/tasks/AddCampeonTask.ts
@@ -2,27 +2,22 @@ import Campeon from '../../domain-layer/entities/Campeon';
 import DatabaseConnection from '../../persistence-layer/DatabaseConnection';
 import IAsyncTask from './IAsyncTask';
 
-export type AddCampeonData = {
-  nombre: string;
-  alias: string;
-  rol: string;
-  dificultad: string;
-  historia: string;
-  imagen: string;
-};
-
-export default class AddCampeonTask implements IAsyncTask<Campeon> {
-  private addCampeonData: AddCampeonData;
-
-  public constructor(addCampeonData: AddCampeonData) {
-    this.addCampeonData = addCampeonData;
+export default class FindCampeonTask implements IAsyncTask<Campeon> {
+  private campeonId: number;
+  
+  public constructor(campeonId: number) {
+    this.campeonId = campeonId;
   }
 
   public async execute(): Promise<Campeon> {
     const databaseConnection = await DatabaseConnection.getInstance();
     const campeonRepository = databaseConnection.getRepository(Campeon);
 
-    const campeon = campeonRepository.save(this.addCampeonData);
+    const campeon = await campeonRepository.findOneBy({ id: this.campeonId });
+
+    if (!campeon) {
+      throw new Error('Campeon not found.');
+    }
 
     return campeon;
   }
